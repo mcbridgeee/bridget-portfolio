66c7efbd8798c12f02e4ec645853bac7c70ff844
mcbridgeee <bridgetmctiernan@gmail.com>
2025-12-18 23:38:14 -0500

    chore: configure for custom domain and fix layout issues

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/style.css');
   eleventyConfig.addPassthroughCopy('src/*.png');
+  eleventyConfig.addPassthroughCopy('src/CNAME');
   return {
     dir: {
       input: 'src',
@@ -9,6 +10,5 @@ module.exports = function (eleventyConfig) {
       output: '_site',
     },
     templateFormats: ['njk', 'md', 'html'],
-    pathPrefix: '/bridget-portfolio/',
   };
 };
